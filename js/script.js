6f92c4ff5c0811e3585e6073206380aa6a588993
CosimoA <cosimo.archip@gmail.com>
2023-11-26 10:02:17 +0100

    CONFIG: setup input & flux in js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Dichiaro variabile preesistente HTML
+const buttonValue = document.getElementById("btnpush");
+const emailUser = document.getElementById("email");
+// Lista mail preesistente nel Database
+
+    //controlla se la mail esiste nel database
+    
+        // prendo gli elementi dell'array
+
+        // Confronto gli elementi con l'imput dell'user
+
+    // output risultato
+
