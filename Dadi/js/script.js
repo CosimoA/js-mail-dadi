77df8dd905ee50ac8651e6eddd2066ba1d6b06c1
CosimoA <cosimo.archip@gmail.com>
2023-11-26 11:09:29 +0100

    CONFIG: js flux

---
+++ Dadi/js/script.js
@@ -0,0 +1,12 @@
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+// Giocatore lancia il dado
+
+// Comuter lancia il dado
+
+// Se il giocatore ha un numero superiore al computer vince
+
+// Se il giocatore ha un numero inferiore al computer perde
+
+// Se i dati sono uguali si verifica il pareggio
